c44c6658c39b6305df072a53c4870af6770159eb
Atharvat77 <Atharvat77@gmail.com>
2021-02-07 10:44:34 +0530

    Rakshit Mario

--- sketch.js
+++ sketch.js
@@ -1,13 +1,36 @@
+var mario,mario_running;
+var bg,bgImage;
 function preload(){
-
+    mario_running=loadAnimation("images/mar1.png","images/mar2.png","images/mar3.png","images/mar4.png","images/mar5.png","images/mar6.png","images/mar7.png");
+    bgImage=loadImage("images/bgnew.jpg");
 }
 
 function setup() {
-createCanvas(1000, 600);
+    createCanvas(1000, 600);
+    bg=createSprite(500,300,580,300);
+    bg.addImage(bgImage);
+    bg.scale=0.5;
+
+    mario=createSprite(200,505,20,50);
+    mario.addAnimation("running",mario_running);
+    mario.scale=0.3;
+
+    ground=createSprite(200,585,400,10);
+    ground.visible=false;
 
 
 }
 
 function draw() {
+    background("black");
 
+    if(keyDown("space"))
+    {
+        mario.velocityY=-16;
+    }
+
+    mario.velocityY=mario.velocityY+0.5;
+    mario.collide(ground);
+
+    drawSprites();
 }
